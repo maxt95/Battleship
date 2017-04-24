7d99c92648514fb42749c1914c064b73ec1f934d
Max Conrad <maxconrad@me.com>
2017-04-24 00:05:36 -0400

    changes

--- joinRoom.php
+++ joinRoom.php
@@ -11,8 +11,7 @@
     $numPlayers = $_SESSION['NumPlayers'];
     $_SESSION['NumPlayers'] = $numPlayers + 1;
 
-    $lives = (int)2;
-    $_SESSION['Lives'] = $lives;
+    
     echo json_encode($_SESSION[$name]);
 
 ?>
